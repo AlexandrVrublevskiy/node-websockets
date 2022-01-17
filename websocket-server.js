55d61a3d91ca9ba3277fe29f81daf88cdd36eb4f
Врублевський Олександр Володимирович <oleksandr.vrublevsky@itmc.zp.gov.ua>
2022-01-17 16:31:31 +0200

    add err logger

--- websocket-server.js
+++ websocket-server.js
@@ -1,10 +1,17 @@
 const http = require("http");
 const express = require( "express");
 const WebSocket = require( "ws");
-
 const app = express();
-
 const server = http.createServer(app);
+
+var winston = require('winston');
+
+  var logger = winston.createLogger({
+    level: 'error',
+    transports: [
+      new (winston.transports.File)({ filename: 'error.log' })
+    ]
+  });
 
 app.use(express.static(__dirname + '/'));
 
@@ -24,17 +31,18 @@ webSocketServer.on('connection', ws => {
     ws.send(JSON.stringify({ "test" : "Hi there, I am a WebSocket server"}));
 });
 
-app.get('/', (req, res) => {
+app.get('/ws', (req, res) => {
       res.sendFile(__dirname + "index.html");
   })
 
 app.post('/api/send-cnt', (req, res) => {
-    console.log(req.body);
     webSocketServer.clients.forEach(client => {
       client.send(JSON.stringify(req.body));
     });
     res.sendStatus(200);
 } )
+
+logger.log('error', 'test error message %s', 'my string');
 
 
 server.listen(3000, () => console.log("Server started"))
