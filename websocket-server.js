c353bd50e6f23caf8b1788f3b4950fe5ec6db1d5
AlexandrVrublevskiy <39702930+AlexandrVrublevskiy@users.noreply.github.com>
2022-02-01 19:05:29 +0200

    Add files via upload

--- websocket-server.js
+++ websocket-server.js
@@ -1,45 +1,74 @@
 const http = require("http");
+const https = require("https");
+const fs = require("fs");
+const basicAuth = require('express-basic-auth')
 const express = require( "express");
 const WebSocket = require( "ws");
 const app = express();
-const server = http.createServer(app);
+//const server = http.createServer(app);
+
+const options = {
+  key: fs.readFileSync("./ssl/priv.key"),
+  cert: fs.readFileSync("./ssl/cert.crt"),
+};
+const httpsServer = https.createServer(options, app);
+
+
 
 var winston = require('winston');
 
-  var logger = winston.createLogger({
+ var logger = winston.createLogger({
     level: 'error',
     transports: [
       new (winston.transports.File)({ filename: 'error.log' })
     ]
   });
+  
+var auth = basicAuth({
+		users: { 'ContactCenter': 'CCpwd2022' }
+      })
+      
+var jsonCntCache;
+  
 
 app.use(express.static(__dirname + '/'));
 
 app.use(express.json());
 
-const webSocketServer = new WebSocket.Server({ server });
+const webSocketServer = new WebSocket.Server({ server: httpsServer, path: '/wss' });
 
 webSocketServer.on('connection', ws => {
     ws.on('message', m => {
-          webSocketServer.clients.forEach(client => {
+         /* webSocketServer.clients.forEach(client => {
             client.send(m)
-          });
+          });*/
+          ws.executorId = JSON.parse(m.toString()).executorId;
+          //console.log(JSON.parse(m.toString()).executorId);
+          webSocketServer.clients.forEach(function each(client) {
+            console.log('Executor.ID: ' + client.executorId);
+        });
     });
 
     ws.on("error", e => {
       logger.log('error', e);
     });
 
-    ws.send('{ "start" : "Successful connection to WebSocket server"}');
+    ws.send(jsonCntCache);
 });
 
-app.get('/ws/', (req, res) => {
-      res.sendFile(__dirname + "index.html");
+
+app.get('/ws', (req, res) => {
+      res.sendFile(__dirname + "/index.html");
 })
 
-app.post('/api/send-cnt', (req, res) => {
+app.post('/api/send-cnt', auth, (req, res) => {
+    jsonCntCache = JSON.stringify(req.body);
     webSocketServer.clients.forEach(client => {
-      client.send(JSON.stringify(req.body));
+    client.send(JSON.stringify(req.body));
+    console.log(client);
+      if (client.executorId === 41) {
+        //webSocketServer.client.executorReqCnt = req.body
+      }
     });
     res.sendStatus(200);
 })
@@ -50,4 +79,4 @@ app.use(function(err, req, res, next) {
   res.status(500).send('Something broke!');
 });
 
-server.listen(3000, () => console.log("Server started"))
+httpsServer.listen(3001, () => console.log("Server started"))
